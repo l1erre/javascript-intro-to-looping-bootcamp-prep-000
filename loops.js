803a656acd729ffb0578f92587e18917d89045a2
Narcita Hamilton <majesticwaterfall11@yahoo.com>
2017-04-04 22:25:05 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,27 +1,24 @@
 function forLoop(array){
-    for   (let i = 0; i < 25; i++)
-      if (i === 1){
+  for   (let i = 0; i < 25; i++){
+    if (i === 1){
       array.push ("I am 1 strange loop." );
     } else {
-        array.push ("I am ${i} strange loop${i === 0 ? : 's'}."
-      return array;
+      array.push (`I am ${i} strange loop${i === 0 ? '' : 's'}`);
     }
+  }
+  return array;
 }
 
-function whileLoop(){
-  let countdown = 100;
-while (countdown > 0)
-  console.log(--countdown);
-   return done;
+function whileLoop(n){
+while (n > 0){
+  console.log(--n)
+  }
+  return 'done' ;
 }
 
 function doWhileLoop(array) {
- function maybeTrue() {
-   return Math.random() >= 0.5;
-}
-
-do {
-array.pop();
-} while (array.length > 0 && maybeTrue());
-
-}
+ do {
+   array.pop();
+   return array;
+ } while ( array.length > 0 && maybeTrue() );
+} 
